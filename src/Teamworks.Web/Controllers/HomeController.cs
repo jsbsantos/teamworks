5672f13f446c622f45b7fcac92ce15c76bba411b
Filipe Pinheiro <filipe.am.pinheiro@gmail.com>
2012-04-27 22:20:28 +0100

    test raven connection

--- src/Teamworks.Web/Controllers/HomeController.cs
+++ src/Teamworks.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
-using Teamworks.Web.Controllers;
+using Raven.Client.Document;
 using Teamworks.Web.Controllers.Api;
+using Teamworks.Web.Models;
 
 namespace TeamWorks.Web.Controllers {
     public class HomeController : Controller {
@@ -8,6 +9,21 @@ namespace TeamWorks.Web.Controllers {
         [ActionName("View")]
         public ActionResult Index() {
             return View(ProjectsController.Projects);
+        }
+
+        [HttpGet]
+        public ActionResult Raven() {
+            using (var store = new DocumentStore() {ConnectionStringName = "RavenDB"}.Initialize()) {
+                using (var session = store.OpenSession()) {
+                    session.Store(new Project()
+                                  {
+                                      Name = "teamworks",
+                                      Description = "Track your projects everywhere"
+                                  });
+                    session.SaveChanges();
+                }
+            }
+            return new ContentResult() {Content = "Project added =)."};
         }
     }
 }
