9f24a4a1c152661461ee3c0cdc5a80e84c2e2b64
Joao Santos <joao.sb.santos@gmail.com>
2012-07-29 16:24:46 +0100

    gantt chart improvements

--- src/Teamworks.Web/Content/js/app/tw.d3.js
+++ src/Teamworks.Web/Content/js/app/tw.d3.js
@@ -1,60 +1,104 @@
 ﻿var TW = TW || { };
-TW.Gantt = function (jsonendpoint) {
-    var r = 960,
-        rw = 770, rh = 150,
-        scale = 5,
-        padX = 50, padY = 15,
-        w = 10, h = 13, headeroffset = 10,
-        boxoffset = padY - h - 0.5;
+TW.Gantt = function (jsonendpoint, duration) {
+    //vertical_ticks - multiples of 5
+    var vertical_ticks = 50, horizontal_ticks = 4,
+        chart_width = $("#chart").width()/*width=100%*/, chart_height = 150,
+        chart_start_padx = 50, chart_start_pady = 15, headeroffset = 10,
+        padX = 0, padY = 2,
+        box_width = 10, box_height = 13, boxoffset = -box_height - 0.5;
+
+    var elementCount = 0, total_duration=0;
 
     var bubble = d3.layout.pack()
-        .sort(null)
-        .size(["100%", rh]);
+        .size(["100%", chart_height]);
 
     var vis = d3.select("#chart").append("svg")
         .attr("width", "100%")
-        .attr("height", rh);
+        .attr("height", chart_height);
 
+    var x = d3.scale.linear()
+            .domain([0, duration || (chart_width - chart_start_padx) / 10])
+            .range([chart_start_padx, chart_width]);
+
+    vis.selectAll("line")
+            .data(x.ticks(vertical_ticks))
+            .enter().insert("line", ":first-child")
+            .attr("x1", x).attr("y1", chart_start_pady)
+            .attr("x2", x).attr("y2", 120)
+            .attr("width", 1)
+            .style("stroke", "#ccc");
+
+    vis.selectAll(".rule")
+            .data(x.ticks(vertical_ticks))
+            .enter().insert("text", ":first-child")
+            .attr("x", x)
+            .attr("y", chart_start_pady)
+            .attr("dy", -3)
+            .attr("text-anchor", "middle")
+            .text(function (d, i) { return d; })
+            .attr("font-size", "0.8em");
+
+    var y = d3.scale.linear()
+            .domain([0, horizontal_ticks * box_height])
+            .range([chart_start_pady, chart_height + chart_start_padx]);
+
+    vis.selectAll("lineh")
+            .data(y.ticks(horizontal_ticks))
+            .enter().insert("line", ":first-child")
+            .attr("x1", chart_start_padx)
+            .attr("y1", function (d, i) {
+                return chart_start_pady + headeroffset + ((box_height + padY) * i) + i;
+            })
+            .attr("x2", function (d, i) {
+                return chart_width;
+            })
+            .attr("y2", function (d, i) {
+                return chart_start_pady + headeroffset + ((box_height + padY) * i) + i;
+            })
+            .attr("width", 1)
+            .style("stroke", "#ccc");
+
+    /*
     //Draw grid vertical lines
     var enter = vis.selectAll("gridlines")
-        .data(d3.range(0, (rw - padX) / scale))
-        .enter();
+    .data(d3.range(0, (rw - padX) / scale))
+    .enter();
 
     var header_x1 = padX - w;
     var header_x2 = padX - w;
 
     enter.append("svg:line")
-        .attr("x1", function (d, i) {
-            return (header_x1 += w);
-        })
-        .attr("y1", padY)
-        .attr("x2", function (d, i) {
-            return (header_x2 += w);
-        })
-        .attr("y2", rh)
-        .style("stroke", "rgb(0,0,0)")
-        .style("stroke-width", 1)
-        .style("stroke-opacity", 0.1);
+    .attr("x1", function(d, i) {
+    return (header_x1 += w);
+    })
+    .attr("y1", padY)
+    .attr("x2", function(d, i) {
+    return (header_x2 += w);
+    })
+    .attr("y2", rh)
+    .style("stroke", "rgb(0,0,0)")
+    .style("stroke-width", 1)
+    .style("stroke-opacity", 0.1);
 
     //draw header text
     enter = vis.selectAll("gridheader")
-        .data(d3.range(0, (rw - padX) / scale, scale))
-        .enter();
+    .data(d3.range(0, (rw - padX) / scale, scale))
+    .enter();
 
     var header_x = padX - (w * 5) - 2;
-    enter.append("text").text(function (d) {
-        return d;
-    }).attr("x", function (d, i) {
-        return (header_x += ((w * 5))) - (d > 9 ? 3.5 : 0);
-    }).attr("y", function (d, i) {
-        return padY;
+    enter.append("text").text(function(d) {
+    return d;
+    }).attr("x", function(d, i) {
+    return (header_x += ((w * 5))) - (d > 9 ? 3.5 : 0);
+    }).attr("y", function(d, i) {
+    return padY;
     })
-        .attr("font-size", "0.8em");
-
-    //draw bars
+    .attr("font-size", "0.8em");
+    */
+    //draw gantt chart bars
     d3.json(jsonendpoint, function (json) {
         var node = vis.selectAll("rect")
-            .data(bubble.nodes(flat(json))
+            .data(bubble.nodes(flatten(json))
                     .filter(function (d) { return !d.children; }))
             .enter();
 
@@ -64,77 +108,87 @@ TW.Gantt = function (jsonendpoint) {
         }).attr("x", function (d, i) {
             return 0;
         }).attr("y", function (d, i) {
-            return d.tw_y + padY;
+            return d.tw_y + padY * i - 2; //2 -> line offset
         })
-        .attr("fill", "black")
-        .attr("font-size", "0.8em");
+            .attr("fill", "black")
+            .attr("font-size", "0.9em");
 
-        //grid horizontal line
-        node.append("svg:line")
-            .attr("x1", 0)
-            .attr("y1", function (d, i) {
-                return d.tw_y + padY;
-            })
-            .attr("x2", rw)
-            .attr("y2", function (d, i) {
-                return d.tw_y + padY;
-            })
-            .style("stroke", "rgb(0,0,0)")
-            .style("stroke-width", 1)
-            .style("stroke-opacity", 0.1);
+        //        //grid horizontal line
+        //        node.append("svg:line")
+        //                .attr("x1", 0)
+        //                .attr("y1", function (d, i) {
+        //                    return d.tw_y + padY * i;
+        //                })
+        //                .attr("x2", chart_width)
+        //                .attr("y2", function (d, i) {
+        //                    return d.tw_y + padY * i;
+        //                })
+        //                .style("stroke", "rgb(0,0,0)")
+        //                .style("stroke-width", 1)
+        //                .style("stroke-opacity", 0.1);
 
         //item duration box
         node.append("rect").attr("x", function (d, i) {
-            return padX + d.tw_x;
-        }).attr("y", function (d) {
-            return d.tw_y+boxoffset;
+            return chart_start_padx + d.tw_x;
+        }).attr("y", function (d, i) {
+            return d.tw_y + boxoffset + padY * i +padY/2; //pady/2 -> ballance padding
         }).attr("width", function (d) {
             return d.tw_w;
         }).attr("height", function (d) {
             return d.tw_h;
         }).attr("rx", 4)
-          .attr("ry", 4);
+            .attr("ry", 4)
+            .style("fill", "#08C")
+            .style("stroke", "rgb(0,0,0)")
+            .style("stroke-width", 1)
+            .style("stroke-opacity", 0.5);
+        ;
 
         //item duration text
         node.append("text").text(function (d) {
             return d.tw_element.duration + "h";
         }).attr("x", function (d, i) {
-            return padX + d.tw_x + 5;
+            return chart_start_padx + d.tw_x + 5;
         }).attr("y", function (d, i) {
-            return d.tw_y + padY - 3;
+            return d.tw_y + padY * i - 3;
         })
-            .attr("fill", "white")
+        .attr("fill", "white")
         .attr("font-size", "0.8em");
-        
+
+        //        var y = d3.scale.linear()
+        //            .domain([0, elementCount * box_height])
+        //            .range([chart_start_pady, chart_height + chart_start_padx]);
+
+        //        vis.selectAll("line")
+        //            .data(y.ticks(elementCount))
+        //            .enter().append("line")
+        //            .attr("x1", 0).attr("y1", chart_start_pady)
+        //            .attr("x2", 0).attr("y2", chart_start_padx + y)
+        //            .attr("width", 1)
+        //            .style("stroke", "red");
+        drawlines();
     });
+
+    
+   
 
     // Returns a flattened hierarchy containing all leaf nodes under the root.
 
-    function flat(root) {
+    function flatten(root) {
 
         var elements = root.elements;
         var relations = root.relations;
         var flattened = [];
-        var elementCount = 0;
-
-        var getDepth = function (element, current) {
-            var curr = current || 0;
-
-            var elem = $.grep(relations, function (e, i) { return e.activity == element; });
-            if (elem.length > 0)
-                return getDepth(elem[0], curr + 1);
-
-            return curr + 1;
-        };
+        
         var lasty = headeroffset;
         $.each(elements, function (index, element) {
             elementCount += 1;
-            lasty = lasty + h;
+            lasty = lasty + box_height;
             var e = {
-                tw_x: w * getDurationOffset(element),
-                tw_y: lasty + elementCount,
-                tw_w: w * element.duration,
-                tw_h: h,
+                tw_x: box_width * getDurationOffset(element),
+                tw_y: lasty + elementCount + chart_start_pady,
+                tw_w: box_width * element.duration,
+                tw_h: box_height,
                 tw_element: element
             };
 
